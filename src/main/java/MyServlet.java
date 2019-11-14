0b21497f156c6e1ef244e9b291ce992396662a9d
Jonas <jonas.rauchhaus@gmail.com>
2019-11-14 19:48:27 +0000

    adding unit testing and logging

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -3,14 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
+    private static final Logger log= Logger.getLogger(MyServlet.class.getName());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello new World!");
+        String message = "Hello, World!";
+        resp.getWriter().write(message);
+        LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
+        log.info(message);
         //System.out.println(resp.getServletPath());
     }
 
